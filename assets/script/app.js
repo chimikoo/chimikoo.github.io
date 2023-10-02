e4eb0c4ed192bd41fe2d834ab08076855353cbb6
Per-Emil Johansson <chimikoo@gmail.com>
2023-10-02 17:02:40 +0200

    added back to top button

--- assets/script/app.js
+++ assets/script/app.js
@@ -16,3 +16,19 @@ socialIcons.forEach((icon) => {
   });
 });
 
+const backToTopButton = document.getElementById("backToTop")
+
+backToTopButton.onclick = () =>
+  document.documentElement.scroll({
+    top: 0,
+    behavior: "smooth"
+  });
+
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 200) {
+    backToTopButton.hidden = false;
+  } else {
+    backToTopButton.hidden = true;
+  }
+};
+
